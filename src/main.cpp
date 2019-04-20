7ac81b82ecdee66ffbe62878c1d66c6d79c2c457
Abdul Zreika <azre6702@uni.sydney.edu.au>
2019-04-20 12:34:21 +1000

    Set up foundations of main file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
+void printUsage() {
+    std::cout << "Usage: punch <FILE>" << std::endl;
+}
+
+void compileProgram(std::string filename, std::ostream& out) {
+    out << "#!/bin/bash" << std::endl;
+    out << std::endl;
+    out << "echo this is the result of " << filename << std::endl;
+}
 
 int main(int argc, char** argv) {
-    std::cout << "👊👊👊" << std::endl;
+    if (argc != 2) {
+        printUsage();
+        return 1;
+    }
+
+    std::stringstream result;
+    compileProgram(argv[1], result);
+
+    std::string outputFilename = "out.sh";
+    std::ofstream outputFile(outputFilename);
+    outputFile << result.str();
+    std::cout << result.str();
+
     return 0;
 }
